34c6034dbca9bb893fcfc8bf8ef638655dfce4d4
Kauã Miranda <kauadecarvalho20@gmail.com>
2025-03-06 20:21:40 -0300

    Removendo um pacientes novos da tabela

---
+++ JavaScript/ex09/remover-pacientes.js
@@ -0,0 +1,10 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+var tabela = document.querySelector("#tabela-pacientes");
+tabela.addEventListener("dblclick", function(event){
+    var alvoEvento = event.target;
+    var paiDoAlvo = alvoEvento.parentNode;
+    paiDoAlvo.remove();
+        
+});
+

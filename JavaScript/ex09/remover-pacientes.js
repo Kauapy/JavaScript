29fca0cd80b4090ab768146b1dd36a5c2ce41fa6
Kauã Miranda <kauadecarvalho20@gmail.com>
2025-03-07 13:23:16 -0300

    Aplicando animação de fade out na <tr>

--- JavaScript/ex09/remover-pacientes.js
+++ JavaScript/ex09/remover-pacientes.js
@@ -3,8 +3,16 @@ var pacientes = document.querySelectorAll(".paciente");
 var tabela = document.querySelector("#tabela-pacientes");
 tabela.addEventListener("dblclick", function(event){
     var alvoEvento = event.target;
-    var paiDoAlvo = alvoEvento.parentNode;
-    paiDoAlvo.remove();
+    var paiDoAlvo = alvoEvento.closest("tr")
+
+
+
+    if(paiDoAlvo){
+        paiDoAlvo.classList.add("fadeOut");
+        setTimeout(function() {
+            paiDoAlvo.remove();
+        }, 500);
         
+    }
 });
 
